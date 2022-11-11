2a0fdeafabd5572b768b348c9762f1450673327e
Rabboni Rabi <rabbonir@gmail.com>
2022-11-11 15:38:09 +0530

    wrote day_wise_school_count_tracking

--- reports_automation/data_trends/school_count_trends.py
+++ reports_automation/data_trends/school_count_trends.py
@@ -16,6 +16,54 @@ import os
 from datetime import date
 from pathlib import Path
 
+def day_wise_school_count_tracking(master_file_name, sheet_name, df_today, group_levels, udise_col):
+    """
+    Function to track the number of schools at given grouping levels on the day the script is run.
+    The day's count is updated in the master tracking excel file.
+
+    Parameters:
+    -----------
+    master_file_name: str
+        Name of the master file with the day wise count of schools
+    sheet_name: str
+        The name of the sheet with the day wise count of schools    
+    df_today: DataFrame
+        Data of school UDISE codes fetched today
+    group_levels: str
+        The columns in the data to group by (Eg: district/educational district/block)
+    udise_col: str
+        The name of the column in the sheet with UDISE values 
+    """
+
+    # Get the full file path to the master trends tracking file - assuming in generated reports folder
+    master_file_path = os.path.join(fileutilities.get_gen_reports_dir_path(), master_file_name)
+
+    # Group the data fetched for today by grouping level, counting the number of UDISE codes
+    df_today_grouped = df_today.groupby(group_levels)[udise_col].count().reset_index
+
+    # Rename the UDISE column with counts
+    df_today_grouped.rename(columns={udise_col: date.today() + 'count'})
+
+    
+    if(os.path.exists(master_file_path)):
+        # If the file exists, update and save the data with today's count
+
+        df_master = pd.read_excel(master_file_path, sheet_name=sheet_name)
+
+        # Merge the data for the day with the master data
+        df_updated = df_master.merge(df_today_grouped)
+
+        return df_updated
+    
+    else:
+        # If file doesnt exist, create data and save for day 1
+        df_master = df_today_grouped
+
+
+    file_utilities.save_to_excel({sheet_name: df_master}, master_file_name)    
+
+
+
 
 def day_wise_tracking(master_file_name, df_today):
     """
@@ -62,7 +110,8 @@ def day_wise_tracking(master_file_name, df_today):
         df_formaster = df_day1.groupby(['District', 'UDISE'])['UDISE'].count()
         df_formaster = pd.ExcelWriter(r'C:\Users\Admin\Downloads\data_trends.xlsx')
         df_formaster.to_excel(df_day1, index = False)
-        df_formaster.save()
+        df_formaster.save()"""
+      
 
 
 
@@ -79,14 +128,16 @@ def main():
     # Alternatively
     # Ask the user to select the School enrollment abstract excel file.
     school_enrollment_abstract = file_utilities.user_sel_excel_filename()
-    df_report = pd.read_excel(school_enrollment_abstract, sheet_name='Report', skiprows=0)
+    df_report = pd.read_excel(school_enrollment_abstract, sheet_name='Report', skiprows=4)
 
 
     # district wise count of schools based on UDISE code - Separate function for this
+    day_wise_school_count_tracking(
+        'school_count_trends.xlsx', 'School count tracking', df_report, ['District'], 'UDISE_Code')
 
     # UDISE day wise tracking (present/absent) - Separate function for this
-    df_today = pd.read_excel(r'C:\Users\Admin\Downloads\Test for Daily.xlsx', sheet_name='Sheet2')
-    day_wise_tracking('data_trends.xlsx', df_today)
+    #df_today = pd.read_excel(r'C:\Users\Admin\Downloads\Test for Daily.xlsx', sheet_name='Sheet2')
+    #day_wise_tracking('data_trends.xlsx', df_today)
 
 
 
