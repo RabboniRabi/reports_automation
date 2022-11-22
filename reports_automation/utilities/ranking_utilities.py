1d1750381a0ec7347ead3aaceaff54b3df64aa52
Rabboni Rabi <rabbonir@gmail.com>
2022-11-22 11:47:23 +0530

    Added percent ranking utilities

---
+++ reports_automation/utilities/ranking_utilities.py
@@ -0,0 +1,48 @@
+"""
+Module with utility functions to perform ranking of data
+"""
+
+def percent_ranking(df, group_levels, agg_cols, agg_func, frac_col_name, num_col, den_col, rank_col_name, sort=False, ascending=True, tie_method='min'):
+    """
+    Function to rank data based on percentage (Value of one column compared to another column)
+    
+    Parameters:
+    -----------
+    df: Pandas DataFrame
+        The raw data
+    group_levels: list
+        The list of columns to group by
+    agg_cols: list
+        The columns to aggregate the grouping by
+    agg_func: str
+        The aggregate function to be applied on the aggregated columns (eg: sum, count, mean)
+    frac_col_name: str
+        The name of a new column with fractional values computed using two columns in the data
+    num_col: str
+        The name of the column whose values will go in the numerator of percent based ranking calculation
+    den_col: str
+        The name of the column whose values will go in the denominator of percent based ranking calculation
+    sort: bool
+        Default: False. Flag to indicate if values need to be sorting when grouping
+    ascending: bool
+        Default: True. Flag to if values need to be ranking by ascending order
+    tie_method: str
+        to rank the group of records that have the same value (i.e. ties)
+
+    Ref: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.rank.html                        
+    
+    Returns:
+    --------
+        The updated DataFrame object with the fractional values used for ranking and the ranking
+    """
+    # Group by grouping levels and aggregate by given columns and aggregate function
+    df_rank = df.groupby(group_levels, as_index=False, sort=sort)[agg_cols].agg(agg_func)
+
+    # Calculate fraction of values (to be used for ranking)
+    df_rank[frac_col_name] = (df_rank[num_col]/df_rank[den_col])
+    df_rank[rank_col_name] = df_rank[frac_col_name].rank(ascending=ascending, method=tie_method)
+    df_rank = df_rank.reset_index()
+
+    return df_rank
+
+
