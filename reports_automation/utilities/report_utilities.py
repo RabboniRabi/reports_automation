8aadb2983fd1f5e7d13f11cedc2840f2194f502a
skandaaramani <skandaa.ramani@gmail.com>
2022-11-25 18:45:41 +0530

    report_utilities.py and common_pool_ceo_new_format updated

--- reports_automation/utilities/report_utilities.py
+++ reports_automation/utilities/report_utilities.py
@@ -4,15 +4,47 @@ Module with utility functions that can be commonly used for different reports.
 
 
 import pandas as pd
-import reports_automation.ceo_reports.ranking as ranking
+#import reports_automation.ceo_reports.ranking as ranking
+import os
+import utilities.file_utilities as file_utilities
 
-def report_summary(raw_data,brc_master_sheet,district_name,deo_user_elm,deo_user_sec,block_name,beo_user,udise_col,
-                        school_category,school_level):
+brc_file_name = 'BRC_CRC_Master_sheet.xlsx'
+brc_master_sheet_name = 'BRC-CRC Updated sheet'
 
-    report_summary = pd.concat([brc_master_sheet[district_name,deo_user_elm,deo_user_sec,block_name,beo_user,udise_col,
-                        school_category,school_level],raw_data], axis=1)
+# Columns to be dropped from the BRC mapping sheet
+brc_master_drop_cols = ['Cluster ID', 'CRC Udise','CRC School Name']
+
+def concat_with_brc_master(raw_data,udise_col):
+    """
+
+    Parameters
+    ----------
+    raw_data
+
+    Returns
+    -------
+
+    """
+
+    brc_master_sheet = get_brc_master()
+    brc_master_sheet = brc_master_sheet.drop(brc_master_drop_cols, axis=1)
+    report_summary = pd.concat([raw_data,brc_master_sheet],keys=udise_col)
+    print(report_summary.columns.to_list())
 
     return report_summary
+
+def get_brc_master():
+    """
+    This function would return the master brc-crc file that would be required for merging with the raw data required
+    in all other reports- CEO review or otherwise.
+
+    """
+    mapping_data_dir = file_utilities.get_mapping_data_dir_path()
+    # read from excel, get sub columns
+    brc_mapping_file_path = os.path.join(mapping_data_dir, brc_file_name)
+    brc_master = pd.read_excel(brc_mapping_file_path,brc_master_sheet_name)
+    return brc_master
+
 
 def get_elementary_report(report_summary):
 
@@ -37,7 +69,7 @@ def get_secondary_report(report_summary):
     This module would return a dataframe with the list of entries that pertain to the secondary schools
     for any report that calls it.
     report_summary: The final dataframe that is returned after creating the specific report
-    deo_elm_ranking: this would be a column that has the BEO ranking for the specific report
+    deo_sec_ranking: this would be a column that has the BEO ranking for the specific report
     """
 
     deo_sec_ranking = ranking.get_ranking(df, deo_user_sec, CP)
@@ -47,9 +79,19 @@ def get_secondary_report(report_summary):
     return secondary_report
 
 
+common_pool_basefile = file_utilities.user_sel_excel_filename()
+raw_data = pd.read_excel(common_pool_basefile, sheet_name='Abstract')
+
+udise_col = 'udise_code'
+
+def main():
+    df_test = concat_with_brc_master(raw_data,udise_col)
+    print(df_test)
+    return df_test
 
 
 
-
+if __name__ == "__main__":
+    main()
 
 
