05ae426602ed57150b5ccf71178ffa40d5b57c90
Rabboni Rabi <rabbonir@gmail.com>
2023-06-05 10:08:53 +0530

    worked on SC/ST report for SSLC students marks report.

--- reports_automation/ad_hoc/sslc_student_marks.py
+++ reports_automation/ad_hoc/sslc_student_marks.py
@@ -23,6 +23,11 @@ agg_dict = {
     '80% and above': "sum"
 }
 
+sc_agg_dict = {
+    "emis_id": "count",
+    "pass" : 'sum'
+}
+
 def total_marks_summary_schoolwise():
     """
 
@@ -74,5 +79,45 @@ def total_marks_summary_schoolwise():
     file_utilities.save_to_excel({'sslc_student_marks': df_data_grouped}, 'sslc_student_marks.xlsx', index=True)
 
 
+def sc_students_marks_summary():
+    """
 
-total_marks_summary_schoolwise()
+    Args:
+        raw_data:
+
+    Returns:
+
+    """
+    # Read the database connection credentials
+    credentials_dict = dbutilities.read_conn_credentials('db_credentials.json')
+
+    # Get the common pool to be surveyed details from the database as a Pandas DataFrame object
+    raw_data = dbutilities.fetch_data_as_df(credentials_dict, 'sslc_sc_student_marks.sql')
+    # Rename the column names to standard format
+    raw_data = column_cleaner.standardise_column_names(raw_data)
+    # Convert all column types to string
+    raw_data = raw_data.astype({
+        "total_marks": "int",
+        "subj1_centum": "int",
+        "subj2_centum": "int",
+        "subj3_centum": "int",
+        "subj4_centum": "int",
+        "subj5_centum": "int",
+        "subj1_pass": "int",
+        "subj2_pass": "int",
+        "subj3_pass": "int",
+        "subj4_pass": "int",
+        "subj5_pass": "int"
+        })
+    #print(raw_data.columns)
+    raw_data['pass'] = (raw_data['subj1_pass'] + raw_data['subj2_pass'] + raw_data['subj3_pass'] + raw_data['subj4_pass'] + raw_data['subj5_pass'] == 5)
+    raw_data['average_marks'] = raw_data['total_marks']/500
+    
+    # Group the data to grouping_level
+    df_data_grouped = raw_data.groupby(['school_management_type'], as_index=False).agg(sc_agg_dict)
+    print(df_data_grouped)
+    #file_utilities.save_to_excel({'sslc_sc_student_marks': df_data_grouped}, 'sslc_sc_student_marks.xlsx', index=True)
+
+
+
+sc_students_marks_summary()
