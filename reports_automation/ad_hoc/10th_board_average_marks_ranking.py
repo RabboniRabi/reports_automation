393c50d384920d93f0d2d560ff5ac55bdf15b66b
Akshayagunasekaran <akshaya.g@theeducationalliance.org>
2023-07-26 11:37:01 +0530

    worked on 10th average marks state-wise ranking, district-wise ranking and splitting the data and saving it in separate files.

--- reports_automation/ad_hoc/10th_board_average_marks_ranking.py
+++ reports_automation/ad_hoc/10th_board_average_marks_ranking.py
@@ -10,7 +10,23 @@ import data_cleaning.column_cleaner as column_cleaner
 import utilities.file_utilities as file_utilities
 import utilities.report_splitter_utilities as report_splitter
 import xlsxwriter
+
+sort_columns = [cols.average_marks, cols.lang_average_marks, cols.eng_average_marks, cols.math_average_marks,
+                cols.science_average_marks, cols.social_average_marks]
+rank_state_column_names = [cols.rank_state, cols.lang_rank_state, cols.eng_rank_state, cols.math_rank_state,
+                     cols.science_rank_state, cols.social_rank_state]
+rank_dist_columns = [cols.rank_dist, cols.lang_rank_dist, cols.eng_rank_dist, cols.math_rank_dist,
+                     cols.science_rank_dist, cols.social_rank_dist]
+
 def colour_coding(average_marks):
+    """
+    Function to format the cells based on a criteria
+    Args:
+        average_marks: column to grade
+
+    Returns:
+    Formatted data
+    """
 
     red = 'background-color: red;'
     orange = 'background-color: orange;'
@@ -29,6 +45,14 @@ def colour_coding(average_marks):
         return default
 
 def _get_overall_school_performance(average_marks):
+    """
+    Function to get overall school performance based on average marks.
+    Args:
+        average_marks: column to grade
+
+    Returns:
+    School-wise performance data
+    """
     if average_marks <= 35:
         return "Poor"
     elif(average_marks > 35) and (average_marks <= 50):
@@ -38,6 +62,39 @@ def _get_overall_school_performance(average_marks):
     else:
         return "Good"
 
+def district_level_ranking(df):
+    """
+    Function to rank the data district-wise
+    Args:
+        data_dict: Pandas Dataframe
+            Dataframe to rank district-wise
+
+    """
+    # Splitting the data based on district
+    data_dict = report_splitter.split_report(df, cols.district_name)
+
+    # Loop to iterate through the dictionary to rank the data district-wise
+    for dist in data_dict.keys():
+        df = data_dict[dist]
+        # Finding the total number of schools in the district since rank should look like 15/175
+        dist_total_schools = df[cols.udise_col].nunique()
+        count = 0
+        # Loop to iterate to sort the columns
+        for sort_col in sort_columns:
+            # Sorting the dataframe based on average marks, subject-wise average marks
+            df.sort_values(by=sort_col, ascending=False, inplace=True)
+            # Ranking district-wise
+            df.insert(count, rank_dist_columns[count], range(1, 1 + len(df)))
+            df[rank_dist_columns[count]] = df[rank_dist_columns[count]].apply(lambda rank_dist: str(rank_dist) + '/' + str(dist_total_schools))
+            # Updating the ranked dataframe to the corresponding district in the dictionary
+            data_dict.update({dist: df})
+            count = count + 1
+        # Deleting the dataframe after updating.
+        del df
+    # Saving the district-wise ranked data
+    report_splitter.save_split_report(data_dict, "10th_average_marks")
+
+
 def state_level_ranking(df):
     """
     Function to rank the data State-wise
@@ -45,19 +102,29 @@ def state_level_ranking(df):
         df: Dataframe to rank
 
     Returns:
-
+    State level ranked dataframe
     """
-    # Sorting the dataframe based on average marks.
-    df.sort_values(by=cols.average_marks, ascending=False, inplace= True)
-    # Finding the total number of unique values in a column since rank should look like 15/6250
-    state_total = df[cols.district_name].nunique()
-    df.insert(0, cols.rank_state, range(1, 1 + len(df)))
-    df.reset_index(inplace=True)
+
+    # Finding the total number of schools in the state since rank should look like 15/6250
+    state_total_schools = df[cols.udise_col].nunique()
+    count = 0
+    # Loop to iterate to sort the columns
+    for sort_col in sort_columns:
+        # Sorting the dataframe based on average marks, subject-wise average marks
+        df.sort_values(by=sort_col, ascending=False, inplace=True)
+        # Ranking state-wise
+        df.insert(count, rank_state_column_names[count], range(1, 1 + len(df)))
+        df[rank_state_column_names[count]] = df[rank_state_column_names[count]].apply(lambda rank_state: str(rank_state) + '/' + str(state_total_schools))
+        count = count+1
+
+    # Get the school performance based on average marks
     df[cols.school_performance] = df[cols.average_marks].apply(_get_overall_school_performance)
-    df.style.apply(colour_coding, subset=["lang_average_marks", "eng_average_marks", "math_average_marks", "science_average_marks", "social_average_marks"], axis=0)
-    data_dict = report_splitter.split_report(df, cols.district_name)
-    report_splitter.save(data_dict)
-    print(df)
+    # Reordering the columns for better readability
+    df = df.iloc[:, [6, 7, 8, 9, 10, 11, 12, 13, 14, 20, 0, 15, 1, 16, 2, 17, 3, 18, 4, 19, 5]]
+
+
+    return df
+
 
 def main():
     """
@@ -66,11 +133,15 @@ def main():
 
     """
     # Read the database connection credentials
-    credentials_dict = dbutilities.read_conn_credentials('db_credentials.json')
+    credentials_dict = dbutilities.read_conn_credentials('db_credentials_rabboni.json')
 
     # Get the 10th government school average marks from the database as a Pandas DataFrame object
     raw_data = dbutilities.fetch_data_as_df(credentials_dict, 'sslc_govt_school_avg_marks.sql')
+    # Rename the column names to standard format
+    raw_data = column_cleaner.standardise_column_names(raw_data)
     # Get the State level ranking
-    state_level_ranking(raw_data)
+    df = state_level_ranking(raw_data)
+    district_level_ranking(df)
 
-main()
+if __name__ == "__main__":
+    main()
--- reports_automation/utilities/report_splitter_utilities.py
+++ reports_automation/utilities/report_splitter_utilities.py
@@ -5,7 +5,7 @@ Module to filter based on a specific column value and save in separate files
 import utilities.file_utilities as file_utilities
 import utilities.column_names_utilities as cols
 import os
-
+import data_cleaning.column_cleaner as column_cleaner
 
 
 def split_report(df_report, column_to_split_on):
@@ -30,18 +30,22 @@ def split_report(df_report, column_to_split_on):
 
     return split_data_dict
 
-def save_split_report(split_df_data):
+def save_split_report(split_df_data,dir_name):
     """
     Function to save the multiple dataframes in a dictionary to an Excel files
-     Parameters
+
+    Parameters
         ---------
-        split_df_data:
+        split_df_data: dict
+            Dictionary with the splitted pandas dataframe is stored
+        dir_name: str
+            The name of the folder want to store the excel files.
     """
     # Loop to iterate through dictionary for saving in separate files
     for key in split_df_data.keys():
 
-        dir_path = file_utilities.get_curr_day_month_gen_reports_dir_path()
-        file_utilities.save_to_excel({'Report': split_df_data[key]}, key + '.xlsx', dir_path=dir_path)
+        dir_path = file_utilities.get_curr_day_month_gen_report_name_dir_path(dir_name)
+        file_utilities.save_to_excel({'Report': split_df_data[key]}, str(key).title() + '.xlsx', dir_path=dir_path)
 
 
 def split_report_given_file(file_name, column_to_split_on, sheet_name, skiprows=0):
@@ -60,7 +64,8 @@ def split_report_given_file(file_name, column_to_split_on, sheet_name, skiprows=
     dir_path = file_utilities.get_curr_month_source_data_dir_path()
     file_path = os.path.join(dir_path, file_name)
     df = file_utilities.read_sheet(file_path, sheet_name, skiprows)
-
+    # Rename the column names to standard format
+    df = column_cleaner.standardise_column_names(df)
     split_data_set = split_report(df, column_to_split_on)
 
     save_split_report(split_data_set)
