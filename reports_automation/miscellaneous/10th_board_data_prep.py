68d32f69c943f9ee699cd0e465e8d8698cae2aaf
Akshayagunasekaran <akshaya.g@theeducationalliance.org>
2023-08-16 18:42:10 +0530

    worked on 10th district level reports

--- reports_automation/miscellaneous/10th_board_data_prep.py
+++ reports_automation/miscellaneous/10th_board_data_prep.py
@@ -1,15 +1,48 @@
 import sys
+
+import pandas as pd
+
 sys.path.append('../')
 import json
 
+import reports.config_reader as config_reader
 import reports.data_fetcher as data_fetcher
-
+import utilities.column_names_utilities as cols
 
 file_open = open("configs/10th_board_results.json", "r")
 config = json.load(file_open)
 source_config = config["source_config"]
 source_data = source_config["sources"]
-type(source_data)
+print(type(source_data[0]))
 df_data = data_fetcher.get_data_from_config(source_data[0])
 print(df_data)
 file_open.close()
+
+def main(raw_data, grouping_levels, agg_dict):
+    """
+    Function to group the student level data to school level data and performing aggregation on certain columns
+
+    Args:
+        raw_data:
+
+    Returns:
+    School-level dataframe
+    """
+    # Grouping the previous academic year student level data to school level
+    prev_ac_yr_raw_data = raw_data.groupby(by=grouping_levels, as_index=False).agg(agg_dict)
+    # Getting the Pass %
+    prev_ac_yr_raw_data[cols.prev_pass_perc] = round(((prev_ac_yr_raw_data[cols.prev_pass]/prev_ac_yr_raw_data[cols.prev_tot_stu]) * 100), 2)
+
+    # Deleting the unnecessary columns
+    prev_ac_yr_raw_data.drop(columns=[cols.prev_pass, cols.prev_tot_stu], inplace=True)
+
+    # Grouping the current academic year student level data to school level
+    curr_ac_yr_raw_data = raw_data.groupby(by=grouping_levels, as_index=False).agg(agg_dict)
+    # Getting the Pass %
+    curr_ac_yr_raw_data[cols.curr_pass_perc] = round(((curr_ac_yr_raw_data[cols.curr_pass]/curr_ac_yr_raw_data[cols.curr_tot_stu])*100), 2)
+
+    # Merging both the datasets
+    raw_data = curr_ac_yr_raw_data.merge(prev_ac_yr_raw_data, how='left', on= grouping_levels)
+
+
+
