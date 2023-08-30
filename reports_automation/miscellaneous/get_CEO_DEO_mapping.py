d703677aada9d398b881621fb0d6bce727becdac
Rabboni Rabi <rabbonir@gmail.com>
2023-08-30 16:46:06 +0530

    Added function to get CEO DEO mapping from BRC mapping

---
+++ reports_automation/miscellaneous/get_CEO_DEO_mapping.py
@@ -0,0 +1,73 @@
+"""
+Script to create a CEO - DEO mapping dictionary from BRC-CRC mapping data
+"""
+
+import sys
+sys.path.append('../')
+
+import utilities.utilities as utilities
+import utilities.file_utilities as file_utilities
+import utilities.column_names_utilities as cols
+
+import json
+
+def get_ceo_deo_mapping():
+    """
+    Function to get the DEOs to CEO mapping from the BRC CRC mapping data
+
+    Returns:
+    --------
+    Dictionary of the CEO to DEO mapping
+    """
+
+    # Fetch the BRC CRC mapping data
+    mapping_data_dir_path = file_utilities.get_mapping_data_dir_path()
+    file_path = file_utilities.get_file_path('BRC_CRC_Master_V3.xlsx', mapping_data_dir_path)
+
+    df_brc_crc_mapping = file_utilities.read_sheet(file_path, sheet_name='BRC-CRC V3 wo hyphen')
+
+    ceo_deo_mapping = {}
+
+    # Get the list of unique CEOs (districts)
+    ceos = df_brc_crc_mapping[cols.district_name].unique()
+
+    # For each ceo, get the unique DEO Elementary and Secondary
+    for ceo in ceos:
+        df_brc_crc_mapping_ceo = utilities.filter_dataframe_column(df_brc_crc_mapping, cols.district_name, [ceo])
+
+        deo_elems = df_brc_crc_mapping_ceo[cols.deo_name_elm].unique()
+        deo_secs = df_brc_crc_mapping_ceo[cols.deo_name_sec].unique()
+
+        ceo_deo_mapping[ceo] = {
+            "Elementary_DEOs" : deo_elems.tolist(),
+            "Seondary_DEOs" : deo_secs.tolist()
+        }
+
+    return ceo_deo_mapping
+
+
+def get_and_save_ceo_deo_mapping():
+    """
+    Function to fetch the CEO to DEO mapping from BRC CRC mapping file
+    and save the resulting mapping dictionary as a JSON file.
+    """
+
+    # Get the mapping
+    ceo_deo_mapping = get_ceo_deo_mapping()
+
+    # Get the path where the JSON needs to be stored
+    mapping_data_dir_path = file_utilities.get_mapping_data_dir_path()
+    file_path = file_utilities.get_file_path('CEO_DEO_mapping.json', mapping_data_dir_path)
+
+    # Write the dictionary to the JSON file
+    with open(file_path, 'w') as file:
+        json.dump(ceo_deo_mapping, file, indent=4)
+
+
+if __name__ == "__main__":
+    get_and_save_ceo_deo_mapping()
+
+
+
+
+
