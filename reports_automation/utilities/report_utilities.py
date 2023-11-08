a1ed5d7f076285083bac272948eaa523f1452da6
Akshayagunasekaran <akshaya.g@theeducationalliance.org>
2023-11-08 16:57:57 +0530

    added new BRC-CRC sheet name to the code

--- reports_automation/utilities/report_utilities.py
+++ reports_automation/utilities/report_utilities.py
@@ -12,10 +12,10 @@ import utilities.column_names_utilities as cols
 from enums.combine_data_types import CombineDataTypes as combine_data_types
 
 brc_file_name = 'BRC_CRC_Master_V3.xlsx'
-brc_master_sheet_name = 'BRC-CRC V3 wo hyphen'
+brc_master_sheet_name = 'BRC-CRC V1'
 
 # Columns to be dropped from the BRC mapping sheet
-brc_master_drop_cols = ['CRC Udise','CRC School Name', 'BRTE']
+brc_master_drop_cols = ['CRC Udise','CRC School Name']#, 'BRTE'
 
 # Define the list of columns to group by for rankings
 beo_ranking_group_cols = [cols.beo_user, cols.beo_name]
