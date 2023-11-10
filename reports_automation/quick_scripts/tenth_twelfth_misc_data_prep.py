963dc4582f453fb920c2c7695ff2ef206c043ac1
Rabboni Rabi <rabbonir@gmail.com>
2023-11-10 12:54:45 +0530

    Minor changes to way data was read.

--- reports_automation/quick_scripts/tenth_twelfth_misc_data_prep.py
+++ reports_automation/quick_scripts/tenth_twelfth_misc_data_prep.py
@@ -5,12 +5,24 @@ import pandas as pd
 import utilities.file_utilities as file_utilities
 
 # The dataframe with student mark details for defined management type
-govt = pd.read_excel(r'C:\Users\TAMILL\Data Reporting\reports\source_data\Oct_23\sslc\SSLC_stud_level_private_2022-23.xlsx')
+
+dir_path = file_utilities.get_curr_month_source_data_dir_path()
+sslc_pvt_data_file_path = file_utilities.get_file_path('SSLC_stud_level_private_2022-23.xlsx', dir_path)
+df_sslc_pvt = file_utilities.read_sheet(sslc_pvt_data_file_path, 'private_2023')
+print("Data successfully read")
+
+sslc_govt_data_file_path = file_utilities.get_file_path('SSLC_stud_level_government_2022-23.xlsx', dir_path)
+df_sslc_govt = file_utilities.read_sheet(sslc_govt_data_file_path, 'govt_2023')
+print("Data successfully read")
+
+sslc_aided_data_file_path = file_utilities.get_file_path('SSLC_stud_level_aided_2022-23.xlsx', dir_path)
+df_sslc_aided = file_utilities.read_sheet(sslc_aided_data_file_path, 'aided_2023')
 print("Data successfully read")
 
 # The dataframe with mother's qualification details
-moth_qual = pd.read_excel(r'C:\Users\TAMILL\Data Reporting\reports\source_data\Oct_23\sslc\sslc_moth_qual.xlsx')
-print("Data successfully read")
+mother_qual_file_path = file_utilities.get_file_path('sslc_moth_qual.xlsx', dir_path)
+df_mother_qual = file_utilities.read_sheet(mother_qual_file_path, 'Sheet0')
+
 
 # The dataframe with mother's occupation details
 moth_occ = pd.read_excel(r'C:\Users\TAMILL\Data Reporting\reports\source_data\Oct_23\sslc\sslc_moth_occ.xlsx')
